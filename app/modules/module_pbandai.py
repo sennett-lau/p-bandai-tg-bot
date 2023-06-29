88df3d0c1eda8b29f9fbe90a7cc180cf7048174a
Sennett Lau <laub1199@gmail.com>
2023-06-30 00:06:56 +0800

    added pbandai module

---
+++ app/modules/module_pbandai.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_item_availability(item_id):
+    item_url = 'https://p-bandai.com/hk/item/'
+
+    combine_url = item_url + item_id
+
+    response = requests.get(combine_url)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # get the h1 with class o-product__name
+    product_name = soup.find('h1', {'class': 'o-product__name'}).text
+
+    # get the button with the id addToCartButton
+    button = soup.find('button', {'id': 'addToCartButton'})
+
+    # check if the button is disabled
+    if button.has_attr('disabled'):
+        return {
+            'name': product_name,
+            'is_available': False
+        }
+
+    return {
+        'name': product_name,
+        'is_available': True
+    }
+    pass
